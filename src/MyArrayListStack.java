99a034ea21f95533f5251829a3b94b392baba666
nek0722 <220439@astanait.edu.kz>
2023-04-29 00:07:28 +0600

    implemented all the methods to MyArrayListStack

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,2 +1,23 @@
-public class MyArrayListStack {
+
+public class MyArrayListStack<T> {
+    MyArrayList<T> stack;
+    public MyArrayListStack(){
+        stack = new MyArrayList<T>();
+    }
+    public void push(T item){
+        stack.add(item);
+    }
+    public T pop(){
+        stack.checkIndex(stack.size()-1);
+        T removedElement = stack.remove(stack.size()-1);
+        return (T) removedElement;
+    }
+    public T peek(){
+        stack.checkIndex(stack.size()-1);
+        return (T) stack.get(stack.size()-1);
+    }
+    public boolean isEmpty(){
+        return stack.size() == 0;
+    }
+
 }
