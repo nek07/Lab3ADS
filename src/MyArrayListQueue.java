87f05d7c24a6d883c61fbcfb7e06fea0a0047b11
nek0722 <220439@astanait.edu.kz>
2023-04-29 23:20:09 +0600

    changed the structure of methods in MyLinkedListQueue

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -1,30 +1,39 @@
+import java.util.EmptyStackException;
 import java.util.NoSuchElementException;
 public class MyArrayListQueue<T> {
     MyArrayList<T> queue;
-    public MyArrayListQueue(){
+
+    public MyArrayListQueue() {
         queue = new MyArrayList<T>();
     }
-    public void enqueue(T item){
+
+    public void enqueue(T item) {
         queue.add(item);
     }
-    public T dequeue(int index){
-        if(index>=queue.size() || index<0){
-            return null;
-        }
-        T removedElement =queue.remove(index);
+
+    public T dequeue(int index) {
+        isEmptyThrowException();
+        T removedElement = queue.remove(index);
         return removedElement;
     }
-    public T peek(){
-        if(queue.size()==0){
-            return null;
-        }
+
+    public T peek() {
+        isEmptyThrowException();
         return queue.get(0);
     }
-    public int size(){
+
+    public int size() {
         return queue.size();
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return queue.size() == 0;
     }
+
+    private void isEmptyThrowException() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+    }
+
 }
