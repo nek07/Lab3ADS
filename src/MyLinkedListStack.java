f486d83e9349d2c778fddd81964b625de213d4c4
nek0722 <220439@astanait.edu.kz>
2023-04-29 23:05:55 +0600

    implemented isEmpty and isEmptyThrowException method in MyLinkedListStack

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,2 +1,21 @@
-public class MyLinkedListStack {
+import java.util.EmptyStackException;
+
+public class MyLinkedListStack<T> {
+    MyLinkedList<T> stack;
+    public MyLinkedListStack(){
+        stack = new MyLinkedList<>();
+    }
+    public void push(T item){
+        stack.add(item,0);
+    }
+    public boolean isEmpty(){
+        return stack.size() == 0;
+    }
+    private void isEmptyThrowException() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+    }
+
+
 }
