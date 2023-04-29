8ef1359e5e1c7aca88f37fa7bbccea6689f27484
nek0722 <220439@astanait.edu.kz>
2023-04-29 21:48:52 +0600

    implemented dequeue and enqueue in MyArrayListQueue

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -1,7 +1,19 @@
+import java.util.NoSuchElementException;
 public class MyArrayListQueue<T> {
     MyArrayList<T> queue;
     public MyArrayListQueue(){
         queue = new MyArrayList<T>();
     }
+    public void enqueue(T item){
+        queue.add(item);
+    }
+    public T dequeue(int index){
+        if(index>=queue.size() || index<0){
+            return null;
+        }
+        T removedElement =queue.remove(index);
+        return removedElement;
+    }
+
 
 }
