746429c0bfb1d9bc7a39ce51960fec7db5ba9b72
nek0722 <220439@astanait.edu.kz>
2023-04-29 23:27:50 +0600

    implemented constructor of MyLinkedListQueue class and enqueue method

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -1,2 +1,12 @@
-public class MyLinkedListQueue {
+import java.util.NoSuchElementException;
+public class MyLinkedListQueue<T> {
+    MyLinkedList<T> queue;
+    public MyLinkedListQueue(){
+        queue = new MyLinkedList<T>();
+    }
+    public void enqueue(T item){
+        queue.add(item);
+    }
+
+
 }
