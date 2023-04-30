d008eabebb3bd36019ab4d8e565b3305b0d5634e
nek0722 <220439@astanait.edu.kz>
2023-04-30 21:57:04 +0600

    created tests to each method in each class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,64 @@
+import java.sql.SQLOutput;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        MyLinkedList<Integer> ll = new MyLinkedList<>();
-        ll.add(10);
-        ll.add(100);
-        ll.add(200);
-        System.out.println(ll.remove(1));
-        System.out.println(ll.remove(1));;
+
+        //      ## MyLinkedListStack
+        System.out.println("MyLinkedListStack");
+        MyLinkedListStack<Integer> linkedStack = new MyLinkedListStack<>();
+        linkedStack.push(1);
+        linkedStack.push(2);
+        linkedStack.push(3);
+        System.out.println(linkedStack.isEmpty());
+        System.out.println(linkedStack.pop());
+        System.out.println(linkedStack.pop());
+        System.out.println(linkedStack.peek());
+        System.out.println(linkedStack.size());
+
+
+
+//      ## MyLinkedListQueue
+        System.out.println("MyLinkedListQueue");
+        MyLinkedListQueue<Integer> linkedQueue = new MyLinkedListQueue<>();
+        linkedQueue.enqueue(1);
+        linkedQueue.enqueue(2);
+        linkedQueue.enqueue(3);
+        System.out.println(linkedQueue.isEmpty());
+        System.out.println(linkedQueue.dequeue());
+        System.out.println(linkedQueue.dequeue());
+        System.out.println(linkedQueue.peek());
+        System.out.println(linkedQueue.size());
+
+
+//      ## MyArrayListStack
+        System.out.println("MyArrayListStack");
+        MyArrayListStack<Integer> arrStack = new MyArrayListStack<>();
+        arrStack.push(1);
+        arrStack.push(2);
+        arrStack.push(3);
+        System.out.println(arrStack.isEmpty());
+        System.out.println(arrStack.pop());
+        System.out.println(arrStack.pop());
+        System.out.println(arrStack.peek());
+        System.out.println(arrStack.size());
+
+
+//      ## MyArrayListQueue
+        System.out.println("MyArrayListQueue");
+        MyArrayListQueue<Integer> arrQueue = new MyArrayListQueue<>();
+        arrQueue.enqueue(1);
+        arrQueue.enqueue(2);
+        arrQueue.enqueue(3);
+        System.out.println(arrQueue.isEmpty());
+        System.out.println(arrQueue.dequeue());
+        System.out.println(arrQueue.dequeue());
+        System.out.println(arrQueue.peek());
+        System.out.println(arrQueue.size());
+
     }
 }
+
+
